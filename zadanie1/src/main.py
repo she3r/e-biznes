81b1e577d6069dcc586bd0299efb622041320e02
Piotr Kubacki <74492669+she3r@users.noreply.github.com>
2025-03-12 14:42:09 +0100

    init zadanie1

---
+++ zadanie1/src/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+
+def main():
+    print('Hello world')
+    print("Python version")
+    print(sys.version)
+    print("Version info.")
+    print(sys.version_info)
+    x = np.array([1,2,3])
+    print(x)
+    
+if __name__ == "__main__":
+    main()
